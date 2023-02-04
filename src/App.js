741e6ae01ddd3d03f24416ac964719244803aa84
Thiago Fernandes <122948678+Thiagofs00@users.noreply.github.com>
2023-02-04 17:00:45 -0300

    Ed01 - type17 Configuração

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p>Cost</p>
     </div>
   );
 }
+
+export default App;
